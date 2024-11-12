451c4eec108cf87be3d89172f8d85123fda2d4cd
ridha bouyahi <ridha.bouyahyi@outlook.com>
2024-11-12 11:23:06 +0000

    Step 4: Add a Back Button to Not Found Page without using BaseController

--- webapp/controller/NotFound.controller.js
+++ webapp/controller/NotFound.controller.js
@@ -1,12 +1,22 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller) {
+    "sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"
+], function (Controller, History) {
     "use strict";
-    return Controller.extend("demonr.controller.NotFound",{
-        onInit:function(){
-            
+    return Controller.extend("demonr.controller.NotFound", {
+        onInit: function () {},
+       
+        onNavBack: function () {
+            var oHistory,
+                oPreviousHash,oRouter;
+            oHistory = History.getInstance();
+            oPreviousHash = oHistory.getPreviousHash();
+            if (oPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                oRouter = this.getOwnerComponent().getRouter();
+                oRouter.navTo("appHome", {}, true);
+            }
         }
-        
     })
-    
+
 });
