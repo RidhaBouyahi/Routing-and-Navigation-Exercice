07f086457a6dd337fe664a56a3acb5990e052447
ridha bouyahi <ridha.bouyahyi@outlook.com>
2024-11-12 08:33:57 +0000

    New Navigation and Routing Exercice

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["demonr/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"demonr/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
