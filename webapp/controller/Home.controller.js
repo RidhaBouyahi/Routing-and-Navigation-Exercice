e0cdac2b35e634da919ab16dbb7d3d9b8813fe3f
ridha bouyahi <ridha.bouyahyi@outlook.com>
2024-11-12 13:58:11 +0000

    Step 5: Display a Target Without Changing the Hash + Reconfiguring the onNavBack function

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,10 +1,9 @@
-sap.ui.define([
-	"demonr/controller/BaseController"
-
-], function (BaseController) {
-	"use strict";
-	return BaseController.extend("demonr.controller.Home", {
-		onInit: function () {
-		}
-	});
+sap.ui.define(["demonr/controller/BaseController"], function (BaseController) {
+    "use strict";
+    return BaseController.extend("demonr.controller.Home", {
+        onInit: function () {},
+        onDisplayNotFound: function () {
+            this.getRouter().getTargets().display("notfound", {fromTarget: "home"});    
+        }
+    });
 });
--- webapp/controller/NotFound.controller.js
+++ webapp/controller/NotFound.controller.js
@@ -1,10 +1,22 @@
-sap.ui.define([
-	"demonr/controller/BaseController"
+sap.ui.define(["demonr/controller/BaseController"], function (BaseController) {
+    "use strict";
+    return BaseController.extend("demonr.controller.NotFound", {
+        onInit: function () {
+            var oRouter,oTarget;
+            oRouter = this.getRouter();
+            oTarget = oRouter.getTarget("notfound");
+            oTarget.attachDisplay(function (oEvent) {
+                this._oData = oEvent.getParameter("data");
+            }, this);
+        },
+        onNavBack: function () {
+            if (this._oData && this._oData.fromTarget) {
+                this.getRouter().getTargets().display(this._oData.fromTarget);
+                delete this._oData.fromTarget;
+                return;
+            }
+            BaseController.Prototype.onNavBack.apply(this,arguments);
+        }
 
-], function (BaseController) {
-	"use strict";
-	return BaseController.extend("demonr.controller.NotFound", {
-		onInit: function () {
-		}
-	});
+    });
 });
