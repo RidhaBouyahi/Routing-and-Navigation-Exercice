fa56a734f4efd7df2db07727ec182c12aa294635
ridha bouyahi <ridha.bouyahyi@outlook.com>
2024-11-21 15:53:56 +0000

    Step 17: Listen to Matched Events of Any Route

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,7 @@
 sap.ui.define([
-	"demonr/controller/BaseController",
-	"sap/base/Log"
+    "demonr/controller/BaseController", "sap/base/Log"
 
-], function (BaseController,Log) {
+], function (BaseController, Log) {
     "use strict";
     return BaseController.extend("demonr.controller.App", {
         onInit: function () { // Définit le niveau de log à INFO pour que les informations au niveau INFO et plus graves soient enregistrées
@@ -18,6 +17,14 @@ sap.ui.define([
                 // Enregistre un message d'information dans le journal pour signaler que la route est invalide
                 Log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
             });
+            oRouter.attachRouteMatched(function (oEvent) { // Récupère le nom de la route qui a été activée par l'utilisateur
+                var sRouteName = oEvent.getParameter("name");
+
+                // Enregistre un message d'information dans le journal pour indiquer que l'utilisateur a accédé à une route
+                // Le message inclut le nom de la route et le timestamp actuel (en millisecondes depuis l'époque)
+                Log.info("user accessed route " + sRouteName + ", timestamp = " + Date.now());
+            });
+
         }
     });
 });
